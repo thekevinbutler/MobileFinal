c71790529d242bfd9af955875936add5e0a14107
TheByrne <byrne.jaymes@gmail.com>
2017-12-06 14:31:31 -0800

    - working on news api
    trying to get news api to work before we move it to it's own pages

--- MobileFinal/MobileFinal/ViewModels/MainPageViewModel.cs
+++ MobileFinal/MobileFinal/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,103 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static MobileFinal.Models.NewsItem;
+using System.Collections.ObjectModel;
+using System.Net.Http;
 
 namespace MobileFinal.ViewModels
 {
-    public class MainPageViewModel : ViewModelBase
+    public class MainPageViewModel : BindableBase, INavigationAware
     {
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
+        public DelegateCommand<NewsArticle> DeleteNewsCommand { get; set; }
+        public DelegateCommand GetNewsForOrgCommand { get; set; }
+        public DelegateCommand<NewsArticle> NavToMoreInfoPageCommand { get; set; }
+
+        private string _hap;
+        public string Hap
         {
-            Title = "Main Page";
+            get { return _hap; }
+            set { SetProperty(ref _hap, value); }
         }
+
+        private string _newsOrgEnteredByUser;
+        public string NewsOrgEnteredByUser
+        {
+            get { return _newsOrgEnteredByUser; }
+            set { SetProperty(ref _newsOrgEnteredByUser, value); }
+        }
+
+        private ObservableCollection<NewsArticle> _newsCollection = new ObservableCollection<NewsArticle>();
+        public ObservableCollection<NewsArticle> NewsCollection
+        {
+            get { return _newsCollection; }
+            set { SetProperty(ref _newsCollection, value); }
+        }
+
+        INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            _navigationService = navigationService;
+           // DeleteNewsCommand = new DelegateCommand<Articles>(DeleteApod);
+            GetNewsForOrgCommand = new DelegateCommand(GetNewsForOrg);
+            NavToMoreInfoPageCommand = new DelegateCommand<NewsArticle>(NavToMoreInfoPage);
+        }
+
+
+
+        private async void NavToMoreInfoPage(NewsArticle newsItem)
+        {
+            var navParams = new NavigationParameters();
+            navParams.Add("NewsItemInfo", newsItem);
+            await _navigationService.NavigateAsync("MoreInfoPage", navParams);
+        }
+
+
+
+        async void GetNewsForOrg()
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri(
+                string.Format(
+                    $"https://newsapi.org/v2/top-headlines?sources={NewsOrgEnteredByUser}&apiKey=b49235adad6d4e0392cfa2970d4de6f4"));
+            var response = await client.GetAsync(uri);
+            NewsArticle newsData = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                newsData = NewsArticle.FromJson(content);
+                Hap = content.ToString();
+            }
+            NewsCollection.Add(newsData);
+
+
+        }
+        public void DeleteApod(NewsArticle newsItem)
+        {
+            NewsCollection.Remove(newsItem);
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+
+
+
+        }
+
+
+
+
     }
 }
