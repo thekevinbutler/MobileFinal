6bdf7aa4f3fe4f950741d9cc910514a4453a13b7
Kevin Butler <Butler.K@live.com>
2017-12-07 12:54:45 -0800

    Added a static class for storing our api keys

--- MobileFinal/MobileFinal.Android/MainActivity.cs
+++ MobileFinal/MobileFinal.Android/MainActivity.cs
@@ -18,6 +18,7 @@ namespace MobileFinal.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+			
         }
     }
 
---
+++ MobileFinal/MobileFinal/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+namespace MobileFinal
+{
+    class Keys
+    {
+		public static Dictionary<String, String> apiKeys = new Dictionary<String, String> {
+			{ "OpenWeatherMap", "e8ff5f451c5c257a59e27fe803168276" }
+		};
+    }
+}
