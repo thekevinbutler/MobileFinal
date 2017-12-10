5e776070bf34c8bf36fc074bce7ada2f32a682ab
TheByrne <byrne.jaymes@gmail.com>
2017-12-09 22:39:29 -0800

    -fix the forecast item
    still crashes when calling api

--- MobileFinal/MobileFinal/Models/ForecastItem.cs
+++ MobileFinal/MobileFinal/Models/ForecastItem.cs
@@ -13,71 +13,105 @@ namespace MobileFinal.Models
     {
         public class City
         {
+            [JsonProperty("id")]
             public int id { get; set; }
+            [JsonProperty("name")]
             public string name { get; set; }
         }
 
         public class Coord
         {
+            [JsonProperty("lon")]
             public double lon { get; set; }
+            [JsonProperty("lat")]
             public double lat { get; set; }
         }
 
         public class Main
         {
+            [JsonProperty("temp")]
             public double temp { get; set; }
+            [JsonProperty("temp_min")]
             public double temp_min { get; set; }
+            [JsonProperty("temp_max")]
             public double temp_max { get; set; }
+            [JsonProperty("pressure")]
             public double pressure { get; set; }
+            [JsonProperty("sea_level")]
             public double sea_level { get; set; }
+            [JsonProperty("grnd_level")]
             public double grnd_level { get; set; }
+            [JsonProperty("humidity")]
             public int humidity { get; set; }
+            [JsonProperty("temp_kf")]
             public double temp_kf { get; set; }
         }
 
         public class Weather
         {
+            [JsonProperty("id")]
             public int id { get; set; }
+            [JsonProperty("main")]
             public string main { get; set; }
+            [JsonProperty("description")]
             public string description { get; set; }
+            [JsonProperty("icon")]
             public string icon { get; set; }
         }
 
         public class Clouds
         {
+            [JsonProperty("all")]
             public int all { get; set; }
         }
 
         public class Wind
         {
+            [JsonProperty("speed")]
             public double speed { get; set; }
+            [JsonProperty("deg")]
             public double deg { get; set; }
         }
 
         public class Sys
         {
+            [JsonProperty("pod")]
             public string pod { get; set; }
         }
 
         public class List
         {
+            [JsonProperty("dt")]
             public int dt { get; set; }
+            [JsonProperty("main")]
             public Main main { get; set; }
+            [JsonProperty("weather")]
             public List<Weather> weather { get; set; }
+            [JsonProperty("clouds")]
             public Clouds clouds { get; set; }
+            [JsonProperty("wind")]
             public Wind wind { get; set; }
+            [JsonProperty("sys")]
             public Sys sys { get; set; }
+            [JsonProperty("dt_txt")]
             public string dt_txt { get; set; }
         }
 
         public partial class ForecastInfo
         {
+            [JsonProperty("city")]
             public City city { get; set; }
+            [JsonProperty("coord")]
             public Coord coord { get; set; }
+            [JsonProperty("country")]
             public string country { get; set; }
+            [JsonProperty("cod")]
             public string cod { get; set; }
+            [JsonProperty("message")]
             public double message { get; set; }
+            [JsonProperty("cnt")]
             public int cnt { get; set; }
+            [JsonProperty("list")]
             public List<List> list { get; set; }
         }
 
